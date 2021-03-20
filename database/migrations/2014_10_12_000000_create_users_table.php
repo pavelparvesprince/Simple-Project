346f82a414277d9012927c775bc3b20d1eb802d6
pavelparvesprince <pavelparves.o@gmail.com>
2021-03-21 00:16:37 +0600

    second commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('rol_id')->index()->unsigned()->nullable();
+            $table->integer('is_active')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
